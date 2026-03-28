f4d9f3f643406d60d193529684b75ebf0296255c
infinilierre <ulukoyecehan@gmail.com>
2026-03-28 21:59:35 +0100

    Update api.py

--- api.py
+++ api.py
@@ -1,4 +1,7 @@
 from health_ai import app
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5000)
+   if __name__ == "__main__":
+    import os
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host='0.0.0.0', port=port)
